f8ff64816cf6ff8c989eb72a896f44797b5d14ab
Rnaseem <ramis.naseem20@gmail.com>
2019-05-22 01:47:30 -0400

    updated html

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,16 +1,17 @@
-{
-    "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-            "scores": [
-                5,
-                1,
-                4,
-                4,
-                5,
-                1,
-                2,
-                5,
-                4,
-                1
-            ]
-}
+const friends = [
+    {
+        "name": "Ahmed",
+        "photo": "https://cbsnews1.cbsistatic.com/hub/i/2018/11/06/0c1af1b8-155a-458e-b105-78f1e7344bf4/2018-11-06t054310z-1334124005-rc1be15a8050-rtrmadp-3-people-sexiest-man.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    }]
---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const friends = require("../data/friends");
+
+
+module.exports = function (app) {
+
+
+    app.get("/", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+
+    app.get("/survey", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/survey.html"))
+    });
+
+    //Respond with the api call
+    app.get("/api/friends", function (req, res) {
+        return res.json(friends);
+    });
+
+    app.get("*", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+
+};
